6adf4eaf6a52c7744cc17c0bccd7d4ecf7039034
SuKid <137023565+Suvijuk@users.noreply.github.com>
2024-08-01 14:18:29 +0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NvbarComponent } from "./nvbar/nvbar.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NvbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/nvbar/nvbar.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-nvbar',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './nvbar.component.html',
+  styleUrl: './nvbar.component.scss'
+})
+export class NvbarComponent {
+
+}
